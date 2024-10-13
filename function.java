b386793686fefe77d00f4b61b98615aa86443531
Devang Parab <devangparab2002@gmail.com>
2024-10-13 16:31:27 +0530

    Update function.java

--- function.java
+++ function.java
@@ -1,14 +1,19 @@
-    public class function {
+import java.util.Scanner;
+
+public class function {
         //A METHOD to calculate sum of 2 numbers - a & b
         public static void sum(int a, int b) {
             int sum = a + b;
-            System.out.println(sum);
+            System.out.println("total sum " +sum);
         }
        
         public static void main(String[] args) {
-           int a = 10;
-           int b = 20;
-            sum(a, b); // Function Call
+            Scanner sc = new Scanner(System.in);
+            System.out.print("enterfirstnumber:" );
+            int a = sc.nextInt();
+            System.out.print("entersecondnumber:");
+            int b = sc.nextInt();
+            sum(a,b); // Function Call
      
         }
      }
