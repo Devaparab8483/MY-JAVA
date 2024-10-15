c37bdca77df49a94d1b139b1f74aa46964b7af60
Devang Parab <devangparab2002@gmail.com>
2024-10-15 23:15:28 +0530

    Create Sum_of_N_nums.java

---
+++ Sum_of_N_nums.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Sum_of_N_nums {
+    public static int Print_sums(int n){
+    int sum=0;
+    for(int i =1;i<=n;i++){
+        if(i%2 == 0){
+        sum= sum +i;
+        }
+    }
+    return sum;
+    }
+    public static void main(String[] args) {
+        Scanner rd = new Scanner(System.in);
+        System.out.print("Enter the value of N: ");
+        int n = rd.nextInt();
+        int sol = Print_sums(n);
+        System.out.print("The sum of 1 to " + n + " is " +sol);
+        
+    }
+
+
+}
