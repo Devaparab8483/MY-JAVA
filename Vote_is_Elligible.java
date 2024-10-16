986415bfc6d55ad26e5fe68ed2b11d7131930ed3
Devang Parab <devangparab2002@gmail.com>
2024-10-16 23:13:37 +0530

    Create Vote_is_Elligible.java

---
+++ Vote_is_Elligible.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Vote_is_Elligible {
+    public static boolean is_Elligible(int age){
+        if(age>18){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        Scanner rd = new Scanner(System.in);
+        System.out.print("ENter your Age: ");
+        int age = rd.nextInt();
+        System.out.print(" Eligible for Voting" +is_Elligible(age));
+
+    }
+
+}
