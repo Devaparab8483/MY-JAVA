f29ed15c4bfeab1f9fde2e638590d4ed39bf9e39
Devang Parab <devangparab2002@gmail.com>
2024-10-16 23:31:08 +0530

    Create power_func.java

---
+++ power_func.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+
+public class power_func {
+   public static void main(String args[]) {
+       System.out.print("Enter x");
+       Scanner sc = new Scanner(System.in);
+       int x = sc.nextInt();
+       System.out.print("Enter n");
+       int n = sc.nextInt();
+
+
+       int result = 1;
+       //Please see that n is not too large or else result will exceed the size of int
+       for(int i=0; i<n; i++) {
+           result = result * x;
+       }
+
+        // double base = 2;
+        // double exponent = 3;
+        // Calculating 2 raised to the power of 3
+        // double result = Math.pow(base, exponent);
+
+
+       System.out.println("x to the power n is : "+ result);
+   }   
+}
+
+
