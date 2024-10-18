1f30c3395f3bd74305904e3eb3b52def12542888
Devang Parab <devangparab2002@gmail.com>
2024-10-18 23:22:08 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,24 +4,23 @@ import java.util.*;
 public class Fibonacci {
    public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
+       System.out.print("ENter n number ");
        int n = sc.nextInt();
       
-       int a = 0, b = 1;
-          
-       System.out.print(a+" ");
-      
-       if(n > 1) {
-           //find nth term
-           for(int i=2; i<=n; i++) {
-               System.out.print(b+" ");
-               //the concept below is called swapping
-               int temp = b;
-               b = a + b;
-               a = temp;
-           }
+       int first_number= 0, second_number=1;
+       int next_num;
 
+       for(int i=1;i<=n;i++){
+        System.out.print(first_number +",");
+        next_num=first_number+second_number;
+        first_number=second_number;
+        second_number= next_num;
 
-           System.out.println();
        }
-   }   
-}
+          
+       
+
+
+           
+       }
+}   
