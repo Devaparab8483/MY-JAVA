480370837a2c2f7e39ebe7611136c08cccfb47d8
Devang Parab <devangparab2002@gmail.com>
2024-10-19 11:49:51 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -2,20 +2,24 @@ import java.util.*;
 
 
 public class GCD {
+    public static int Findgcd(int n1,int n2){
+        int gcd=1;
+        for (int i=1;i<=n1 && i<=n2;i++){
+            if (n1 % i==0 && n2 % i==0){
+            gcd=i;
+            }
+        }
+        return gcd;
+    }
    public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the first number ");
        int n1 = sc.nextInt();
+       System.out.print("enter Second number ");
        int n2 = sc.nextInt();
 
-
-       while(n1 != n2) { 
-           if(n1>n2) {
-               n1 = n1 - n2;
-           } else {
-               n2 = n2 - n1;
-           }
-       }
-       System.out.println("GCD is : "+ n2);
+       int ans=Findgcd(n1, n2);
+       System.out.println("GCD is : " +ans);
    }   
 }
 
