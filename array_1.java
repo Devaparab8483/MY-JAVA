e675c5d43e3cb88b6aff872d85da5bdf75f0669e
Devang Parab <devangparab2002@gmail.com>
2024-10-23 22:52:05 +0530

    Update array_1.java

--- array_1.java
+++ array_1.java
@@ -1,21 +1,23 @@
 ///Taking an array as an input and printing its elements.
-
 import java.util.*;
 public class array_1 {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the size of array: ");
+       int size = sc.nextInt();
+       int numbers[] = new int[size];  // array declare
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int size=sc.nextInt();
-        int[] number=new int[size];
-        for(int i=0;i<size;i++){
-            number[i]=sc.nextInt();
-        }
+       //taking numbers in array
+       System.out.print("Enter the number: ");
+       for(int i=0; i<size; i++) {
+           numbers[i] = sc.nextInt();
+       }
 
-        //print number in array
-        for(int i=0;i<number.length;i++){
-            System.out.print(number[i]+" ");
-        }
-        }
-        
 
+       //print the numbers in array
+       System.out.print("output: ");
+       for(int i=0; i<numbers.length; i++) {
+           System.out.print(numbers[i]+" ");
+       }
+   }
 }
