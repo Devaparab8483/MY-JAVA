c752a6fdb6605dc2bb75fc9367ade635eab448e2
Devang Parab <devangparab2002@gmail.com>
2024-10-27 12:58:42 +0530

    Update check_sorted_array.java

--- check_sorted_array.java
+++ check_sorted_array.java
@@ -1,3 +1,25 @@
 public class check_sorted_array {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};  // Example array
 
+        boolean isAscending = true;
+        boolean isDescending = true;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] > numbers[i + 1]) {
+                isAscending = false;
+            }
+            if (numbers[i] < numbers[i + 1]) {
+                isDescending = false;
+            }
+        }
+
+        if (isAscending) {
+            System.out.println("The array is sorted in ascending order.");
+        } else if (isDescending) {
+            System.out.println("The array is sorted in descending order.");
+        } else {
+            System.out.println("The array is not sorted.");
+        }
+    }
 }
