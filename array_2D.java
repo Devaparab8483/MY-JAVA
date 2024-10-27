d418319bb6ed2ae60409533beeb67f5aa959c689
Devang Parab <devangparab2002@gmail.com>
2024-10-27 22:49:22 +0530

    Update array_2D.java

--- array_2D.java
+++ array_2D.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class array_2D {
    public static void main(String args[]) {
        Scanner sc = new Scanner(System.in);
+       System.out.print("Enter Number of rows: ");
        int rows = sc.nextInt();
+       System.out.print("Enter Number of columns: ");
        int cols = sc.nextInt();
 
 
@@ -15,12 +17,14 @@ public class array_2D {
        //rows
        for(int i=0; i<rows; i++) {
            //columns
+           System.out.println("Enter the " + (i+1) + " rows number: ");
            for(int j=0; j<cols; j++) {
                numbers[i][j] = sc.nextInt();
            }
        }
 
        for(int i=0; i<rows; i++) {
+        System.out.print("2D matrix : ");
            for(int j=0; j<cols; j++) {
                    System.out.print(numbers[i][j]+" ");
                }
