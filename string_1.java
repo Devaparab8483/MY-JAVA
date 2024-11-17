5377f2d56b7c5853596b17ce8a30d3a0837782e7
Devang Parab <devangparab2002@gmail.com>
2024-11-17 23:42:55 +0530

    Update string_1.java

--- string_1.java
+++ string_1.java
@@ -1,11 +1,20 @@
+
+import java.util.Scanner;
+
 public class string_1 {
     public static void main(String[] args) {
         // string declaration
-        String s1="hello";
-        String s2="hello";
-        String sentence="hello world WHO ARE YOU ?";
+        // String s1="hello";
+        // String s2="hello";
+        // String sentence="hello world WHO ARE YOU ?";
 
         //string input
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your name ");
+        String name = sc.next();
+        //String name1 = sc.nextLine();
+        System.out.println("Hello "+name);
 
 }
 }
