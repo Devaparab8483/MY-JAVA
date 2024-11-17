4893034ad9743af468a690f295c047f65a839499
Devang Parab <devangparab2002@gmail.com>
2024-11-17 22:25:09 +0530

    Update string_1.java

--- string_1.java
+++ string_1.java
@@ -1,8 +1,11 @@
 public class string_1 {
     public static void main(String[] args) {
+        // string declaration
         String s1="hello";
         String s2="hello";
         String sentence="hello world WHO ARE YOU ?";
+
+        //string input
 
 }
 }
