b659bfbb41858fc5e6c0795fc2917112d67ea89d
Devang Parab <devangparab2002@gmail.com>
2024-11-17 22:24:25 +0530

    Create string_1.java

---
+++ string_1.java
@@ -0,0 +1,8 @@
+public class string_1 {
+    public static void main(String[] args) {
+        String s1="hello";
+        String s2="hello";
+        String sentence="hello world WHO ARE YOU ?";
+
+}
+}
