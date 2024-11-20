c50c0309c39f063316f5de19cd08a04e6a50b57c
Devang Parab <devangparab2002@gmail.com>
2024-11-20 23:56:38 +0530

    Update Str_SetBuilder.java

--- Str_SetBuilder.java
+++ Str_SetBuilder.java
@@ -1,8 +1,18 @@
 public class Str_SetBuilder {
     public static void main(String[] args) {
        // Declaration
-        StringBuilder sb = new StringBuilder("hello");
+        StringBuilder sb = new StringBuilder("Devang");
         System.out.println(sb);
+
+        //Get A Character from Index
+        //Set Char
+        System.out.println(sb.charAt(1));
+
+        //Get Char
+        sb.setCharAt(0, 'T');
+        System.out.println(sb);
+
+
     }
 
 }
