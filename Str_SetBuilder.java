7dec132f66be153383f377137538d4f82d4e812f
Devang Parab <devangparab2002@gmail.com>
2024-11-20 23:53:53 +0530

    Create Str_SetBuilder.java

---
+++ Str_SetBuilder.java
@@ -0,0 +1,8 @@
+public class Str_SetBuilder {
+    public static void main(String[] args) {
+       // Declaration
+        StringBuilder sb = new StringBuilder("hello");
+        System.out.println(sb);
+    }
+
+}
