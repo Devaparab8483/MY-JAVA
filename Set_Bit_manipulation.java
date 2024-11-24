289506345f6dfb4e4bcc83a7ef51f2c7e9e83925
Devang Parab <devangparab2002@gmail.com>
2024-11-24 17:19:43 +0530

    Update Set_Bit_manipulation.java

--- Set_Bit_manipulation.java
+++ Set_Bit_manipulation.java
@@ -1,5 +1,9 @@
 public class Set_Bit_manipulation {
     public static void main(String[] args) {
+
+        //Bit mask=1<<position
+        //operation =OR
+
         int n = 5; //0101
         int pos = 1;
         int bitMask = 1<<pos;
