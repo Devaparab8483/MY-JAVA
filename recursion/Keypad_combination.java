b7f4a128e36bbdcc32fd5d64d6e5301ba876b3ce
Devang Parab <devangparab2002@gmail.com>
2025-02-15 23:20:54 +0530

    Update Keypad_combination.java

--- recursion/Keypad_combination.java
+++ recursion/Keypad_combination.java
@@ -19,3 +19,15 @@ public class Keypad_combination {
     }
    
 }
+
+// number and alphabates combination
+/* 0 -> .;
+1 -> abc
+2 -> def
+3 -> ghi
+4 -> jkl
+5 -> mno
+6 -> pqrs
+7 -> tu
+8 -> vwx
+9 -> yz */
