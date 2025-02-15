6764433f9bf1f297e2d904b10337466d9cabccc8
Devang Parab <devangparab2002@gmail.com>
2025-02-15 23:19:26 +0530

    Update Keypad_combination.java

--- recursion/Keypad_combination.java
+++ recursion/Keypad_combination.java
@@ -1,5 +1,21 @@
 package recursion;
 
 public class Keypad_combination {
+    public static String keypad[] = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs","tu", "vwx", "yz"};
+    public static void printKeypadCombination(String number, int idx, String res) {
+        if(idx == number.length()) {
+            System.out.println(res);
+            return;
+        }
 
+        for(int i=0; i<keypad[number.charAt(idx)-'0'].length(); i++) {
+            char curr = keypad[number.charAt(idx)-'0'].charAt(i);
+            printKeypadCombination(number, idx+1, res+curr);
+        }
+    }
+    public static void main(String args[]) {
+        String number = "43";  //press the buttons
+        printKeypadCombination(number, 0, "");
+    }
+   
 }
